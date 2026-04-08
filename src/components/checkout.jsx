690099ea90fe32e258a1cdba9444d5b69e5159b9
Kelvin-Mutugi <mutugikelvin316@gmail.com>
2026-04-08 22:33:08 +0300

    pushe latest code

---
+++ src/components/checkout.jsx
@@ -0,0 +1,11 @@
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Checkout Page</h1>
+      <p>Review your items and proceed to payment.</p>
+    </div>
+  );
+}
+
+export default Checkout;
