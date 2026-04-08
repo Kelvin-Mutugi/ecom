38bdb6344a2c1a524bd4cb24eb79648a07c1abc3
Kelvin-Mutugi <mutugikelvin316@gmail.com>
2026-04-08 22:35:35 +0300

    full commit

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -4,9 +4,9 @@ import ProductCard from "./productCard";
 function ProductList() {
     return(
         <div className="product-list">
-         <ProductCard product={{ name: 'Product 1', price: 29.99, image: 'https://via.placeholder.com/150' }} />
-         <ProductCard product={{ name: 'Product 2', price: 39.99, image: 'https://via.placeholder.com/150' }} />
-         <ProductCard product={{ name: 'Product 3', price: 19.99, image: 'https://via.placeholder.com/150' }} />   
+         <ProductCard product={{id: 1, name: 'Product 1', price: 29.99, image: 'https://via.placeholder.com/150' }} />
+         <ProductCard product={{id: 2, name: 'Product 2', price: 39.99, image: 'https://via.placeholder.com/150' }} />
+         <ProductCard product={{id: 3, name: 'Product 3', price: 19.99, image: 'https://via.placeholder.com/150' }} />   
         </div>
     );
 }
