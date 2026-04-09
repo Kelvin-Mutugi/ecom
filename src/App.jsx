c799c94aa572abe2b4fceefcd245cdf5abdf1bd5
Kelvin-Mutugi <mutugikelvin316@gmail.com>
2026-04-09 17:16:21 +0300

    checkout setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ProductList from './components/productList';
 import { useCart } from './context/CartContext';
 import Cart from './components/cart';
+import Checkout from './components/checkout';
 
 function App() {
   const { cartItems, cartCount } = useCart();
@@ -14,6 +15,7 @@ function App() {
          <p>Items in cart: {cartCount}</p>
         <pre>{JSON.stringify(cartItems, null, 2)}</pre> */}
         <Cart />
+        <Checkout />
 
         <h1>Welcome to My Store</h1>
         <ProductList />
